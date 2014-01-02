de680a1406d3bb68c02bb70ffe4f775f3069548b
Brickgao <brickgao@gmail.com>
2014-01-02 23:48:23 +0800

    add getList

--- src/pop3.py
+++ src/pop3.py
@@ -8,6 +8,7 @@ class pop3:
         
         self.url = url
         self.port = port
+        self.loginSucc = False
         self.logger = logging.Logger(__name__)
 
 
@@ -47,7 +48,25 @@ class pop3:
             return False, _
         else:
             self.logger.info(_[:-2])
+            self.loginSucc = True
             return True, _
+
+    def getList(self):
+
+        if not self.loginSucc:
+            self.logger.error('You should login first')
+            return False, 'You should login first'
+
+        self.sock.sendall('LIST\r\n')
+        self.logger.info('LIST')
+
+        _ = self.sock.recv(1024)
+        if not '+OK' in _:
+            self.logger.error(_[:-2])
+            return False, _
+        else:
+            self.logger.info(_[:-2])
+            return True, _[4:-2].split('\r\n')[:-1]
 
 
 
@@ -60,3 +79,4 @@ if __name__ == '__main__':
     pop.logger.addHandler(ch)
     _user, _pass = raw_input().split(' ')
     pop.login(_user, _pass)
+    pop.getList()
