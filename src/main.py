a0a784d0360776cbb549943e88322910f38fbe53
Brickgao <brickgao@gmail.com>
2014-01-04 22:21:25 +0800

    add main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# -*- coding:utf8 -*-
+
+from PyQt4 import QtGui
+import ui, sys
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = ui.mainWindow()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+
+    main()
